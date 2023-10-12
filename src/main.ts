ca99c3a6a20d4066cd78908d0df7f1f653fc153e
lsy969999 <lsy969999@gmail.com>
2023-10-13 04:25:17 +0900

    csp

--- src/main.ts
+++ src/main.ts
@@ -87,10 +87,35 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
+  /* 
+      none : 어떳 것도 허용하지 않음
+    self : 현재 출처에서는 허용하지만 하위 도메인에서는 허용되지 않음
+    unsafe-inline : 인라인 자바스크립트, 인라인 스타일을 허용
+    unsafe-eval	: eval과 같은 텍스트 자바스크립트 메커니즘을 허용 
+      */
+  // 구글 API 도메인과 인라인 스크립트, eval 스크립트를 허용
+  const cspOptions = {
+    directives: {
+      // 헬멧 기본 옵션 가져오기
+      ...helmet.contentSecurityPolicy.getDefaultDirectives(), // 기본 헬멧 설정 객체를 리턴하는 함수를 받아 전개 연산자로 삽입
+      'script-src': [
+        "'self'",
+        '*.googleapis.com',
+        "'unsafe-inline'",
+        "'unsafe-eval'",
+      ],
+      // 다음과 카카오에서 이미지 소스를 허용
+      'img-src': ["'self'", 'data:', '*.daumcdn.net', '*.kakaocdn.net'],
+      // 소스에 https와 http 허용
+      //'base-uri': ['/', 'http:'],
+    },
+  };
   //helmet
-  app.use(helmet());
-
+  app.use(
+    helmet({
+      contentSecurityPolicy: cspOptions,
+    }),
+  );
   //hbs view
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
